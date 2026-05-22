6d945849b04f31c005cc7c6870d430c2c50dbe7c
khadija-sajjad <khadijasajjad224@gmail.com>
2026-05-22 11:51:07 +0500

    Update sql_analysis.py

--- project 3/sql_analysis.py
+++ project 3/sql_analysis.py
@@ -2,32 +2,32 @@ SELECT * FROM decodelabs_p3.cleaned_data;
 USE decodelabs_p3;
 SELECT COUNT(*) AS Total_Orders 
 FROM cleaned_data;
--- Query 2: Orders by Category
+-- Orders by Category
 SELECT Product, 
        COUNT(*) AS Total_Orders
 FROM cleaned_data
 GROUP BY Product
 ORDER BY Total_Orders DESC;
--- Query 3: Revenue by Product
+--  Revenue by Product
 SELECT Product,
        SUM(TotalPrice) AS Total_Revenue,
        AVG(TotalPrice) AS Avg_Revenue
 FROM cleaned_data
 GROUP BY Product
 ORDER BY Total_Revenue DESC;
--- Query 4: Payment Method Analysis
+-- Payment Method Analysis
 SELECT PaymentMethod,
        COUNT(*) AS Total_Orders
 FROM cleaned_data
 GROUP BY PaymentMethod
 ORDER BY Total_Orders DESC;
--- Query 5: Order Status
+-- Order Status
 SELECT OrderStatus,
        COUNT(*) AS Total_Orders
 FROM cleaned_data
 GROUP BY OrderStatus
 ORDER BY Total_Orders DESC;
--- Query 6: Top Referral Source
+-- Top Referral Source
 SELECT ReferralSource,
        COUNT(*) AS Total_Orders
 FROM cleaned_data
